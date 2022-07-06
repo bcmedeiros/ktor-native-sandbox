29fb16e1b54b183a5e33646bf686aa41eed79d70
Bruno Medeiros <brunojcm@gmail.com>
2022-07-06 17:48:23 +0800

    add ktor hello world

--- build.gradle.kts
+++ build.gradle.kts
@@ -40,7 +40,12 @@ kotlin {
     }
 
     sourceSets {
-        val commonMain by getting
+        val commonMain by getting {
+            dependencies {
+                implementation("io.ktor:ktor-server-core:2.0.3")
+                implementation("io.ktor:ktor-server-cio:2.0.3")
+            }
+        }
         val commonTest by getting
     }
 }
--- src/commonMain/kotlin/Main.kt
+++ src/commonMain/kotlin/Main.kt
@@ -1,4 +1,20 @@
-fun main() {
-    // TODO add ktor
-    println("Hello, Kotlin/Native!")
+import io.ktor.server.application.*
+import io.ktor.server.cio.*
+import io.ktor.server.engine.*
+import io.ktor.server.response.*
+import io.ktor.server.routing.*
+import kotlinx.coroutines.runBlocking
+
+fun main(): Unit = runBlocking {
+    println("Starting Ktor...")
+
+    val server = embeddedServer(CIO, port = 8080) {
+        routing {
+            get("/") {
+                call.respond("Hello, world, from Ktor/Native!")
+            }
+        }
+    }
+
+    server.start()
 }
