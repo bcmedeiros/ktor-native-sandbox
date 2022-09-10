16ad7a886da6eb414c30ea7d3232d3754111275a
Bruno Medeiros <brunojcm@gmail.com>
2022-09-10 11:53:18 +0800

    upgrade to ktor 2.1.1

--- build.gradle.kts
+++ build.gradle.kts
@@ -41,7 +41,7 @@ kotlin {
     }
 
     sourceSets {
-        val ktorVersion = "2.1.0"
+        val ktorVersion = "2.1.1"
         val commonMain by getting {
             dependencies {
                 implementation("io.ktor:ktor-server-core:$ktorVersion")
