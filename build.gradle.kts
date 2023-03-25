6c755dd446e1f5c6d7b8b6e6d7e4e340d7675046
Bruno Medeiros <brunojcm@gmail.com>
2023-03-25 22:08:59 +0800

    feat(ktor): upgrade to 2.2.4

--- build.gradle.kts
+++ build.gradle.kts
@@ -41,7 +41,7 @@ kotlin {
     }
 
     sourceSets {
-        val ktorVersion = "2.2.3"
+        val ktorVersion = "2.2.4"
         val commonMain by getting {
             dependencies {
                 implementation("io.ktor:ktor-server-core:$ktorVersion")
