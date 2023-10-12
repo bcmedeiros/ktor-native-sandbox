f128e0ad0e88d0c97e28126dd53a5f46a3a87291
Bruno Medeiros <brunojcm@gmail.com>
2023-10-12 09:45:04 +0800

    feat(kotlin): upgrade to 1.9.10

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
-    kotlin("multiplatform") version "1.8.0"
-    kotlin("plugin.serialization") version "1.8.0"
+    kotlin("multiplatform") version "1.9.10"
+    kotlin("plugin.serialization") version "1.9.10"
 }
 
 group = "dev.bcmedeiros"
